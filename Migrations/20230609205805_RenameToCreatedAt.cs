78b8ab6ec972efb31503c451ccc0607c974f05f0
Ashkan Taravati <ashkan.taravati@gmail.com>
2023-06-10 22:24:06 +0330

    rename createAt to createdAt

---
+++ Migrations/20230609205805_RenameToCreatedAt.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LicoriceBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameToCreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreateAt",
+                table: "Walls",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreateAt",
+                table: "Cubes",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreateAt",
+                table: "Cards",
+                newName: "CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Walls",
+                newName: "CreateAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Cubes",
+                newName: "CreateAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Cards",
+                newName: "CreateAt");
+        }
+    }
+}
